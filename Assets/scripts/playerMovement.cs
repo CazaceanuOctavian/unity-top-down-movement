aa20b57b8b57430011e86e1327effe4440820013
Cazaceanu Octavian <o.cazatavian@gmail.com>
2023-12-31 17:25:34 +0200

    fixed movement script and added cinnemachine addon

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -7,10 +7,12 @@ public class playerMovement : MonoBehaviour
     private Rigidbody rb; 
     private Transform playerTransform;
 
-    [SerializeField] private float speedVar;
+    [SerializeField] private float walkModifier;
     [SerializeField] private float speedModifier;
     [SerializeField] private float rotationSpeed;
-    [SerializeField] private float rotationSpeed2;
+    [SerializeField] private float aimRoationSpeed;
+
+    
     
     void Start()
     {
@@ -22,23 +24,29 @@ public class playerMovement : MonoBehaviour
     void FixedUpdate()
     {
        movePlayer();
-       
+
        if(Input.GetMouseButton(1)) {
             rotatePlayerToMouse();
-       }
-       else {
-            rotatePlayerToDirection();
        }
     }
 
     void movePlayer () {
-        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speedVar * Time.fixedDeltaTime;
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * walkModifier * Time.deltaTime;
         
-        if(Input.GetKey(KeyCode.LeftShift)) {
-            rb.velocity *= speedModifier * Time.fixedDeltaTime;
+        //transform.Translate(movementDirection * speedVar * Time.deltaTime, Space.World);
+
+        if(rb.velocity != Vector3.zero && !Input.GetMouseButton(1)) {
+            Quaternion toRotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            rb.velocity *= speedModifier * Time.deltaTime;
+        }
+        
         if(Input.GetMouseButton(1)) {
-            rb.velocity /= speedModifier * Time.fixedDeltaTime;
+            rb.velocity /= speedModifier * Time.deltaTime;
         }
         
     }
@@ -50,42 +58,6 @@ public class playerMovement : MonoBehaviour
         Vector3 directionToMouse = targetPosition - transform.position;
         float angle = Mathf.Atan2(directionToMouse.x, directionToMouse.y) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
-    }
-
-    //TODO --> clean up code
-    void rotatePlayerToDirection() {
-        if(Input.GetKey(KeyCode.W)) {
-            Quaternion zRotation = Quaternion.AngleAxis(0, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        else if(Input.GetKey(KeyCode.S)) {
-            Quaternion zRotation = Quaternion.AngleAxis(180, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        else if(Input.GetKey(KeyCode.D)) {
-            Quaternion zRotation = Quaternion.AngleAxis(90, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        else if(Input.GetKey(KeyCode.A)) {
-            Quaternion zRotation = Quaternion.AngleAxis(270, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) {
-            Quaternion zRotation = Quaternion.AngleAxis(45, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) {
-            Quaternion zRotation = Quaternion.AngleAxis(315, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-        if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) {
-            Quaternion zRotation = Quaternion.AngleAxis(135, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
-         if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) {
-            Quaternion zRotation = Quaternion.AngleAxis(225, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, rotationSpeed2 * Time.fixedDeltaTime);
-        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, aimRoationSpeed * Time.fixedDeltaTime);
     }
 }
